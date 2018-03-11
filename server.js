09e687aabc084bfdb88997b322fe19514dd3524d
vaishalisec97 <35034458+vaishalisec97@users.noreply.github.com>
2018-03-11 15:40:49 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'content.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
