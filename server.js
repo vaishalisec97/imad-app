f937fba0cad74b46ec11643ec2f3a174c24e4fc2
vaishalisec97 <35034458+vaishalisec97@users.noreply.github.com>
2018-03-17 09:46:15 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/article one',function(req,res){
+    res.sendFile('article one requested will be sent back' ));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
